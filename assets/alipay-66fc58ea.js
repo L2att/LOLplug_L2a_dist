2db11c6fd13bca4d7b688586fdb17eda2d771f40
L2a <76792101+L2att@users.noreply.github.com>
2025-03-15 06:00:09 +0800

    Delete assets directory

--- assets/alipay-66fc58ea.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/wechat-9463b6f8.jpg",a="/assets/alipay-9a5918cd.jpg";export{s as _,a};
--- assets/alipay-9f9c6b68.js
+++
@@ -1,1 +0,0 @@
-const t=""+new URL("wechat-9463b6f8.jpg",import.meta.url).href,a=""+new URL("alipay-9a5918cd.jpg",import.meta.url).href;export{t as _,a};
